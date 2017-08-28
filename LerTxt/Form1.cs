5b8c1072df6ec481293b0cdbec1040adee82f1b9
GabrielPorto <gabriel_porto_lopes@hotmail.com>
2017-08-27 23:46:14 -0300

    Ler e Escrever TXT

--- LerTxt/Form1.cs
+++ LerTxt/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//add
+using System.IO;
 
 namespace LerTxt
 {
@@ -15,6 +17,37 @@ namespace LerTxt
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "c:\\test.txt";
+            textBox1.Clear();
+
+            string[] linesW = { "First line 11", "Second line 22", "Third line 33" };
+
+            File.WriteAllLines(path, linesW);
+
+            string[] linesR = File.ReadAllLines(path);
+
+            for (int i = 0; i < linesR.Length; i++)
+            {
+                textBox1.Text += "\r\n" + linesR[i];
+            }
+
+
+            StreamReader sr = new StreamReader(path);
+            string texto = sr.ReadToEnd();
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter(path);
+            //sw.WriteLine(linesW);
+            sw.WriteLine(texto);
+
+            sw.Close();
+
+
+
         }
     }
 }
